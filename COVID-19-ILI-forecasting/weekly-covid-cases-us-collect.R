57d7bd0dbea209091e35cf87e07283f850857a17
tierney6 <tierney06@gmail.com>
2020-03-19 20:17:18 -0400

    added NYC data

--- COVID-19-ILI-forecasting/weekly-covid-cases-us-collect.R
+++ COVID-19-ILI-forecasting/weekly-covid-cases-us-collect.R
@@ -9,8 +9,13 @@ us_cases <- read.csv("https://raw.githubusercontent.com/CEIDatUGA/COVID-19-DATA/
 us_state_code <- read.csv("https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv", stringsAsFactors = F)
 us_state_code <- rbind(us_state_code, c("Puerto Rico", "PR"),  c("Virgin Islands", "VI"), c("Guam", "GU"))
 
+# county level data from https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/
+# need to get counts for NYC, forecasted separately from NY 
+# download csv from html, not sure if this link is persistent
+county_cases <- read.csv("https://static.usafacts.org/public/data/covid-19/covid_confirmed_usafacts.csv", stringsAsFactors = F)
 
-format_cases <- function(us_cases, us_state_code){
+
+format_cases <- function(us_cases = us_cases, us_state_code = us_state_code, county_cases = county_cases){
   
   # getting rid of row of country-level counts
   us_cases2 <- us_cases %>% filter(Date != "Total") %>% 
@@ -22,7 +27,7 @@ format_cases <- function(us_cases, us_state_code){
     mutate(date_time_accessed = max(as.POSIXct(time_last_update), na.rm = T)) %>% 
     rename(State_Code = State) %>%
     # remove unwanted columns
-    select(State_Code, Date, state_daily_cases, national_cml_cases = Conf_Cml, national_cml_deaths = Death_Cml, national_cml_rec = Rec_Cml, date_time_accessed) %>%
+    select(State_Code, Date, state_daily_cases, national_cml_cases = Conf_Cml, national_cml_deaths = deaths_Cml, national_cml_rec = Rec_Cml, date_time_accessed) %>%
     # fill in zeros for days with no cases
     mutate_at(c("state_daily_cases", "national_cml_cases", "national_cml_deaths", "national_cml_rec"),~ifelse(is.na(.), 0, .))
   
@@ -30,6 +35,18 @@ format_cases <- function(us_cases, us_state_code){
   us_cases3 <- left_join(us_cases2, us_state_code, by = c("State_Code" = "Abbreviation")) %>%
     rename(location = State) %>% 
     select(location, everything()) 
+  
+  nyc_cases2 <- county_cases %>% 
+    filter(County.Name == "New York City") %>% 
+    gather(key = "Date", value = "NYC_cumulative", -c(countyFIPS:stateFIPS)) %>% 
+    # convert Date to correct date format
+    mutate_at("Date",  ~ as.Date(gsub(pattern = "X", replace = "", .), format = "%m.%d.%Y")) %>%
+    mutate(date_time_accessed = Sys.time()) %>%
+    mutate(state_daily_cases = NYC_cumulative - lag(NYC_cumulative, default = 0)) %>%
+    select(location = County.Name, State_Code = State, Date, state_daily_cases, date_time_accessed) 
+  
+  # add rows for location = New York City
+  us_cases4 <- full_join(us_cases3, nyc_cases2)
   
   # sum daily new cases to get weekly new cases
   fun_add_weekly_cases <- function(x){
@@ -56,18 +73,37 @@ format_cases <- function(us_cases, us_state_code){
     return(tmp3)
     
   }
-  us_cases_weekly <- fun_add_weekly_cases(us_cases3)
+  us_cases_weekly <- fun_add_weekly_cases(us_cases4)
   
   
   return(us_cases_weekly)
   
 }
 
-us_cases_data <- format_cases(us_cases, us_state_code)
+us_cases_data <- format_cases(us_cases, us_state_code, county_cases)
 
 us_cases_data_weekly <- us_cases_data %>% 
   select(-Date, -c(state_daily_cases:national_cml_rec)) %>% 
   group_by(location, WeekStart) %>% 
   distinct()
 
-write.csv(us_cases_data_weekly, "~/COVID19-ILI-forecasting/data/us_cases_data_weekly.csv")
+# subtract NYC cases from NY state cases
+sep_nyc_cases <- function(us_cases_data_weekly = us_cases_data_weekly) {
+  ny_cases <- us_cases_data_weekly %>% 
+    filter(location == "New York") 
+  nyc_cases <- us_cases_data_weekly %>% 
+    filter(location == "New York City") %>% 
+    rename(nyc_weekly_cases = state_weekly_cases) %>% 
+    select(WeekStart, nyc_weekly_cases, NYC = location)
+  tmp <- left_join(ny_cases, nyc_cases, by = "WeekStart") %>% 
+    mutate(state_weekly_cases = state_weekly_cases - nyc_weekly_cases) %>%
+    select(-nyc_weekly_cases, -NYC)
+  
+  tmp2 <- us_cases_data_weekly %>% filter(location != "New York")
+  tmp3 <- rbind(tmp, tmp2)
+  return(tmp3) %>% arrange(location, WeekStart)
+}
+
+us_cases_data_weekly2 <- sep_nyc_cases(us_cases_data_weekly)
+
+write.csv(us_cases_data_weekly2, "COVID-19-ILI-forecasting/data/us_cases_data_weekly.csv")
