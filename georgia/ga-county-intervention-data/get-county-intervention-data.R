5579f843a816afb65b0098833fb3a03537b22963
rlrichards <robbielrichards@gmail.com>
2020-03-27 23:05:21 -0400

    data updates

--- georgia/ga-county-intervention-data/get-county-intervention-data.R
+++ georgia/ga-county-intervention-data/get-county-intervention-data.R
@@ -11,12 +11,11 @@ library(lubridate)
 
 # Set user info and read in google sheet
 options(gargle_oauth_email = "robbielrichards@gmail.com")
-df <- read_sheet("https://docs.google.com/spreadsheets/d/1UZMWpbebhI3HS2BwzK0PCUxAbypYnM9oA9t9jP6zNXE/edit#gid=1772124213", range = "ga-intervention-announcements")
-  
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1UZMWpbebhI3HS2BwzK0PCUxAbypYnM9oA9t9jP6zNXE/edit#gid=1772124213", range = "ga-intervention-announcements", col_types= "c")
 
 # Set start_date column to a date object
 df <- df %>%  
-  mutate(start_date = ymd(as.character(start_date)))
+  mutate(start_date = ymd(as.character(start_date)), announcement_date = ymd(as.character(announcement_date)), end_date = ymd(end_date))
 
 
 
