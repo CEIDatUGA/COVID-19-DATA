1154716c672a61932528f4369ae52e0aa399daa7
Liliana Salvador <salvador@uga.edu>
2020-04-13 20:43:15 -0400

    fixing minor bug with column order

--- georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
+++ georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
@@ -43,15 +43,16 @@ df  <- data.frame(date = day_reported,
                 time_reported = time_reported,
                 cases_cumulative = as.numeric(str_extract(table_cases[2,2], "[0-9]+")), 
                 fatalities_cumulative = as.numeric(str_extract(table_cases[4,2], "[0-9]+")), 
-                hospitalizations_cumulative = as.numeric(str_extract(table_cases[3,2], "[0-9]+")),
                 tests_cumulative = as.numeric(table_tests$X3[2])+as.numeric(table_tests$X3[3]),
+                hospitalizations_cumulative = as.numeric(str_extract(table_cases[3,2], "[0-9]+")),
                 new_cases = NA,
                 new_fatalities = NA,
                 new_tests = NA,
                 new_hospitalizations = NA,
                 source_cases = as.character(url),
                 source_fatalities = as.character(url),
-                source_tests = as.character(url)
+                source_tests = as.character(url),
+                source_hospitalizations = as.character(url)
                  )
 
 # Read existent status report table
@@ -64,8 +65,8 @@ nrows <- dim(table)[1]
 
 # if there is already an entry for that day, it will replace that row by the latest page update
 day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%m/%d/%Y")
-if(identical(day_last_row, as.character(date_reported))){
-    cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests")   
+if(identical(day_last_row, as.character(day_reported))){
+    cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests", "source_hospitalizations")   
     df[,cols] <- apply(df[,cols], 2, function(x) (as.character(x)))
     table[nrows,] <- df
 } else{
@@ -81,6 +82,7 @@ table <- table %>%
   mutate(source_cases = as.character(source_cases), 
          source_fatalities = as.character(source_fatalities), 
          source_tests = as.character(source_tests),
+         source_hospitalizations = as.character(source_hospitalizations),
          new_cases = cases_cumulative - lag(cases_cumulative, default = first(cases_cumulative)),
          new_fatalities = fatalities_cumulative - lag(fatalities_cumulative, default = first(fatalities_cumulative)),
          new_tests = tests_cumulative - lag(tests_cumulative, default = first(tests_cumulative)),
