8e55b17ad2efa89a1b726fb2e7e7a783e28613fc
Liliana Salvador <salvador@uga.edu>
2020-04-20 16:33:25 -0400

    Change to ISO date

--- georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
+++ georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
@@ -36,7 +36,9 @@ table_race <- read_table_from_web(webtext, xpath_race)
 date_reported <- str_extract(as.character(webtext), "[0-9]+\\/[0-9]+\\/[0-9]+ [0-9]+\\:[0-9]+\\:[0-9]+")
 aux <- strsplit(date_reported, " ")
 day <- aux[[1]][1]
-day_reported <- aux[[1]][1]
+day_reported <- unlist(lapply(aux[[1]][1], function(x) format(as.Date(parse_date_time(x,"mdy")), format = "%Y-%m-%d")))
+
+                    
 time_reported <- aux[[1]][2]
 
 df  <- data.frame(date = day_reported,
@@ -60,11 +62,11 @@ table <- read.csv("georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GD
 # remove first column
 table <- table[,-1]
 
-table$date <- unlist(lapply(table$date, function(x) format(as.Date(parse_date_time(x,"mdy")), format = "%m/%d/%y")))
+table$date <- unlist(lapply(table$date, function(x) format(as.Date(parse_date_time(x,"mdy")), format = "%Y-%m-%d")))
 nrows <- dim(table)[1]              
 
 # if there is already an entry for that day, it will replace that row by the latest page update
-day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%m/%d/%Y")
+day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%Y-%m-%d")
 if(identical(day_last_row, as.character(day_reported))){
     cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests", "source_hospitalizations")   
     df[,cols] <- apply(df[,cols], 2, function(x) (as.character(x)))
@@ -102,7 +104,6 @@ table$new_tests[start_tests] <- table$tests_cumulative[start_tests]
 
 start_hospitalizations <- which.min(is.na(table$hospitalizations_cumulative))
 table$new_hospitalizations[start_hospitalizations] <- table$hospitalizations_cumulative[start_hospitalizations]
-
 
 
 write.csv(table, "georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH.csv")
