78a6c461992c5346507d2d73032df592e66df942
Liliana Salvador <salvador@uga.edu>
2020-04-21 16:52:56 -0400

    fixing date column

--- georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
+++ georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
@@ -13,7 +13,7 @@ read_table_from_web <- function(wiki_pop, table_xpath) {
   cases <- wiki_pop %>%
     html_nodes(xpath=table_xpath) %>%
     html_table(fill=TRUE)
-    cases <- cases[[1]]
+  cases <- cases[[1]]
   return(cases)
 }
 
@@ -24,7 +24,7 @@ xpath_counties <- '//*[@id="summary"]/table[2]'
 xpath_tests <- '//*[@id="testing"]/table'
 xpath_deaths <- '//*[@id="deaths"]/table' 
 xpath_race <- '//*[@id="race2"]/table'
-  
+
 webtext <- read_html(url)
 
 table_cases <- read_table_from_web(webtext, xpath_cases)
@@ -37,48 +37,50 @@ date_reported <- str_extract(as.character(webtext), "[0-9]+\\/[0-9]+\\/[0-9]+ [0
 aux <- strsplit(date_reported, " ")
 day <- aux[[1]][1]
 day_reported <- unlist(lapply(aux[[1]][1], function(x) format(as.Date(parse_date_time(x,"mdy")), format = "%Y-%m-%d")))
-
-                    
 time_reported <- aux[[1]][2]
 
 df  <- data.frame(date = day_reported,
-                time_reported = time_reported,
-                cases_cumulative = as.numeric(str_extract(table_cases[2,2], "[0-9]+")), 
-                fatalities_cumulative = as.numeric(str_extract(table_cases[4,2], "[0-9]+")), 
-                tests_cumulative = as.numeric(table_tests$X3[2])+as.numeric(table_tests$X3[3]),
-                hospitalizations_cumulative = as.numeric(str_extract(table_cases[3,2], "[0-9]+")),
-                new_cases = NA,
-                new_fatalities = NA,
-                new_tests = NA,
-                new_hospitalizations = NA,
-                source_cases = as.character(url),
-                source_fatalities = as.character(url),
-                source_tests = as.character(url),
-                source_hospitalizations = as.character(url)
-                 )
+                  time_reported = time_reported,
+                  cases_cumulative = as.numeric(str_extract(table_cases[2,2], "[0-9]+")), 
+                  fatalities_cumulative = as.numeric(str_extract(table_cases[4,2], "[0-9]+")), 
+                  tests_cumulative = as.numeric(table_tests$X3[2])+as.numeric(table_tests$X3[3]),
+                  hospitalizations_cumulative = as.numeric(str_extract(table_cases[3,2], "[0-9]+")),
+                  new_cases = NA,
+                  new_fatalities = NA,
+                  new_tests = NA,
+                  new_hospitalizations = NA,
+                  source_cases = as.character(url),
+                  source_fatalities = as.character(url),
+                  source_tests = as.character(url),
+                  source_hospitalizations = as.character(url)
+)
 
 # Read existent status report table
-table <- read.csv("georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+table <- read.csv("georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH3.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+
 # remove first column
 table <- table[,-1]
 
+# changing date to ISO format
 table$date <- unlist(lapply(table$date, function(x) format(as.Date(parse_date_time(x,"mdy")), format = "%Y-%m-%d")))
 nrows <- dim(table)[1]              
 
 # if there is already an entry for that day, it will replace that row by the latest page update
-day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%Y-%m-%d")
-if(identical(day_last_row, as.character(day_reported))){
-    cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests", "source_hospitalizations")   
-    df[,cols] <- apply(df[,cols], 2, function(x) (as.character(x)))
-    table[nrows,] <- df
+#day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%Y-%m-%d")
+day_last_row <- table$date[nrows]
+if(identical(day_last_row, day_reported)){
+  # if(identical(day_last_row, as.character(day_reported))){
+  cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests", "source_hospitalizations")   
+  df[,cols] <- apply(df[,cols], 2, function(x) (as.character(x)))
+  table[nrows,] <- df
 } else{
-   table <- rbind(table, df) # if not, it will add the report for that day
+  table <- rbind(table, df) # if not, it will add the report for that day
 }
 
 # convert number of cases/fatalities/tests/hospitalizations to numeric class
 num_cols <- c("cases_cumulative", "fatalities_cumulative", "tests_cumulative", "hospitalizations_cumulative", "new_cases", "new_fatalities", "new_tests", "new_hospitalizations")
 table[, num_cols] <- table[, num_cols] %>%
- mutate_if(is.character, as.numeric, na.rm = TRUE) 
+  mutate_if(is.character, as.numeric, na.rm = TRUE) 
 
 min(which(!is.na(table$cases_cumulative)))
 
@@ -134,4 +136,3 @@ table_race <- table_race[-1, ]
 names(table_race) <- names
 table_race$Latest_Status_Report <- date_reported
 write.csv(table_race, "georgia/ga_GDPH_daily_status_report/GA_cases_race_ethnicity_GDPH.csv")
-
