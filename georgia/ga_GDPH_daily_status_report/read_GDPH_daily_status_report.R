43a78edf9c03bd629b94a393e73098beab424a4e
Liliana Salvador <salvador@uga.edu>
2020-04-21 16:58:19 -0400

    clean up comment lines

--- georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
+++ georgia/ga_GDPH_daily_status_report/read_GDPH_daily_status_report.R
@@ -56,7 +56,7 @@ df  <- data.frame(date = day_reported,
 )
 
 # Read existent status report table
-table <- read.csv("georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH3.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+table <- read.csv("georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
 
 # remove first column
 table <- table[,-1]
@@ -66,10 +66,8 @@ table$date <- unlist(lapply(table$date, function(x) format(as.Date(parse_date_ti
 nrows <- dim(table)[1]              
 
 # if there is already an entry for that day, it will replace that row by the latest page update
-#day_last_row <- format(as.Date(parse_date_time(table$date[nrows],"mdy")), format = "%Y-%m-%d")
 day_last_row <- table$date[nrows]
 if(identical(day_last_row, day_reported)){
-  # if(identical(day_last_row, as.character(day_reported))){
   cols <- c("date", "time_reported", "source_cases", "source_fatalities", "source_tests", "source_hospitalizations")   
   df[,cols] <- apply(df[,cols], 2, function(x) (as.character(x)))
   table[nrows,] <- df
@@ -81,8 +79,6 @@ if(identical(day_last_row, day_reported)){
 num_cols <- c("cases_cumulative", "fatalities_cumulative", "tests_cumulative", "hospitalizations_cumulative", "new_cases", "new_fatalities", "new_tests", "new_hospitalizations")
 table[, num_cols] <- table[, num_cols] %>%
   mutate_if(is.character, as.numeric, na.rm = TRUE) 
-
-min(which(!is.na(table$cases_cumulative)))
 
 table <- table %>%
   mutate(source_cases = as.character(source_cases), 
@@ -106,7 +102,6 @@ table$new_tests[start_tests] <- table$tests_cumulative[start_tests]
 
 start_hospitalizations <- which.min(is.na(table$hospitalizations_cumulative))
 table$new_hospitalizations[start_hospitalizations] <- table$hospitalizations_cumulative[start_hospitalizations]
-
 
 write.csv(table, "georgia/ga_GDPH_daily_status_report/GA_daily_status_report_GDPH.csv")
 
