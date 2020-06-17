bee8aa56c476d403619c26f0ade3aafd0cbe33bb
mvevans89 <mvevans@uga.edu>
2020-06-17 07:23:57 -0400

    save dxy html in standard date ISO

--- china/China_casedata/download-dxy-html.R
+++ china/China_casedata/download-dxy-html.R
@@ -6,7 +6,6 @@
 library(httr)
 library(xml2)
 library(rvest)
-library(stringr)
 library(here)
 
 # set sub directory
@@ -17,9 +16,6 @@ url = "https://ncov.dxy.cn/ncovh5/view/pneumonia"
 get_object = GET(url)
 cat(content(get_object, "text"), file="temp.html")
 html_object = read_html(url)
-write_xml(html_object, file=paste("dxy-html-archive/",
-                                  str_sub(get_object$headers$date, 6, 7),
-                                  toupper(str_sub(get_object$headers$date, 9, 11)),
-                                  str_sub(get_object$headers$date, 18, 19),
-                                  str_sub(get_object$headers$date, 21, 22),
-                                  ".html",sep = "")) 
+write_xml(html_object, file=paste0("dxy-html-archive/", substr(get_object$date, 1, 10), "_",
+                                   substr(get_object$date,12,13), 
+                                   substr(get_object$date,15,16),".html")) 
